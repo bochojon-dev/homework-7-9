2a212b55b8c262d367c7ae1f4219261f67d69168
Bochojon-dev <maniyevmaqadjon9@gmail.com>
2024-05-25 00:45:35 +0500

    navbar and hero

---
+++ src/app/home/page.jsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/hero/Hero";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "../hero/Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="container">
+        <div className="hero_contents">
+          <div className="hero_texts">
+            <h4>
+              Posted on <span>STARTUP</span>
+            </h4>
+            <h3>Step-by-step guide to choosing great font pairs</h3>
+            <p>
+              By <span>James West</span> | May 23, 2022{" "}
+            </p>
+            <p>
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident.
+            </p>
+            <button>Read More</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
